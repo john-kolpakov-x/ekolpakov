2523b33c81c4082f372e626b736915b679d1a1f2
ekolpakov <ekolpakov@greet-go.com>
2019-10-10 21:47:50 +0600

    Заработал html custom components

--- client/src/components/combo-single/combo.single.ts
+++ client/src/components/combo-single/combo.single.ts
@@ -5,11 +5,45 @@ class ComboSingle extends HTMLElement {
 
   constructor() {
     super();
-    console.log("create combo-single", this)
+
+    console.log("g423vv5 constructor :: this.close = ", this["close"]);
   }
 
+  main: HTMLSpanElement;
+  selector: HTMLSpanElement;
+
   connectedCallback() {
-    this.innerHTML = "asd";
+
+    this.main = document.createElement("span");
+    this.selector = document.createElement("span");
+
+    console.log("  this.childElementCount = ", this.childElementCount);
+
+    var observer = new MutationObserver(function(mutations) {
+      mutations.forEach(function(mutation) {
+        //Detect <img> insertion
+        if (mutation.addedNodes.length)
+          console.info('Node added: ', mutation.addedNodes[0])
+      })
+    });
+
+    observer.observe(this, { childList: true });
+  }
+
+  static get observedAttributes() {
+    return ["wow"];
+  }
+
+  attributeChangedCallback(name: string, prevValue, newValue) {
+    console.log("attributeChangedCallback " + name, prevValue, newValue);
+  }
+
+  attachedCallback() {
+    console.log("g43v4 :: attachedCallback");
+  }
+
+  adoptedCallback() {
+    console.log("542365v :: adoptedCallback");
   }
 
 }
